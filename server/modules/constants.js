0e67c6e66cfd1199e27a2fdb0413e8e6e267025b
Jackz <me@jackz.me>
2020-04-24 15:19:55 -0500

    fix undefined error if no BANNED_IPS env

--- server/modules/constants.js
+++ server/modules/constants.js
@@ -7,6 +7,6 @@ module.exports = {
     VIDEO_INFO_CACHE:  new Map(),
     VIDEO_HITS:  new Map(),
     OPEN_DOWNLOADS: new Map(),
-    BANNED_IP_LIST: process.env.BANNED_IPS.split(","),
+    BANNED_IP_LIST: process.env.BANNED_IPS ? process.env.BANNED_IPS.split(",") : [],
     VERSION
 }
